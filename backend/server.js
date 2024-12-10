87346a8e1098340bc703709580987a3f0a959e53
Alex7342 <alexborgovan@yahoo.com>
2024-12-10 13:31:30 +0200

    commented predict routed in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { connectDB } = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
-const predictRoutes = require('./routes/predictRoutes');
+//const predictRoutes = require('./routes/predictRoutes');
 
 const app = express();
 app.use(cors());
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/predict', predictRoutes);
+//app.use('/api/predict', predictRoutes);
 
 const PORT = process.env.PORT || 5000;
 
